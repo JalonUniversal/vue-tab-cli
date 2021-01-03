8e5ae8452ebe16fb120b64efb5a7430402b8f2f8
JalonUniversal <453741411@qq.com>
2021-01-04 02:23:18 +0800

    feat: 引入更多图表组件

--- src/lib/echarts.js
+++ src/lib/echarts.js
@@ -1,8 +1,13 @@
  import echarts from 'echarts/lib/echarts';
 
+ // 通用组件
  import 'echarts/lib/component/title';
  import 'echarts/lib/component/legend';
  import 'echarts/lib/component/tooltip';
- import 'echarts/lib/chart/line';
+
+ // 图表组件
+ import 'echarts/lib/chart/line'; // 拆线图
+ import 'echarts/lib/chart/bar'; // 饼图
+ import 'echarts/lib/chart/radar'; // 雷达图
 
  export default echarts;
